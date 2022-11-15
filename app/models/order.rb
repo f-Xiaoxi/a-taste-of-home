86a6668aa2f44600aa4a6b0903db52aebcfb5812
Xing Ern Low <lxingern@gmail.com>
2022-11-15 19:14:49 +0800

    Update model associations to support buyers and sellers

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
   belongs_to :meal
+  has_one :user, through: :meal
 
   validates :status, inclusion: { in: %w[pending accepted rejected completed] }, presence: true
 end
