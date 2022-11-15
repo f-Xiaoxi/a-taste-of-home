704b92e3cfa64d09e4096bd93da93f75d189b51d
Xing Ern Low <lxingern@gmail.com>
2022-11-15 16:29:42 +0800

    seed meals

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Meal.destroy_all
+
+user = User.last
+
+10.times do
+  Meal.create!(
+    name: Faker::Food.dish,
+    description: Faker::Food.description,
+    price: rand(0.1..20).round(2),
+    user: user
+  )
+end
+
+puts "10 meals created"
