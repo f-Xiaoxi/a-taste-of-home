dbb6ee91ce5c12dc06f058293a15a69b6604095d
Xing Ern Low <lxingern@gmail.com>
2022-11-15 16:48:18 +0800

    add meals controller

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,29 @@
+class MealsController < ApplicationController
+  def index
+    @meals = Meal.all
+  end
+
+  def show
+    @meal = Meal.find(params[:id])
+  end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    @meal.user = current_user
+    if @meal.save
+      redirect_to meals_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :price, :description)
+  end
+end
