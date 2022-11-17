4496102fde02c4511afb8ca9d22dfe209c25a76f
juliette barbay <barbayjuliette95@gmail.com>
2022-11-17 14:02:12 +0800

    If not logged in, I can still see the show view from meals

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,5 +1,5 @@
 class MealsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @meals = Meal.all
